7e1ab03c39378e6b8e2181fbaec84d7a07d5981a
Kirill Kainov <kainov.dev@gmail.com>
2023-04-02 16:24:35 +0300

    задание 1

---
+++ Life/AppConfig.cs
@@ -0,0 +1,8 @@
+﻿public class AppConfig
+{
+    public string LoadFilePath { get; set; }
+    public int BoardHeight { get; set; }
+    public int BoardWidth { get; set; }
+    public int BoardCellSize { get; set; }
+    public double BoardLiveDensity { get; set; }
+}
